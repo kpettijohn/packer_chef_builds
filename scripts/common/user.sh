a8f2cfdfa2502514f950af632294e409141695a6
Jason Wieringa <jason@jasonwieringa.com>
2015-07-21 13:57:49 -0400

    Copy ssh key from local
    + Adds packer file provisioner to copy public ssh key to machine
    + Modifys scripts/user.sh to use copied file
    + Adds docs to README for setting path to ssh key

--- scripts/common/user.sh
+++ scripts/common/user.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 mkdir ~opsadmin/.ssh
-wget --no-check-certificate \
-    'https://s3-us-west-2.amazonaws.com/esri-pdx-public/pub_key/id_packer_builds_rsa.pub' \
-    -O ~opsadmin/.ssh/authorized_keys
+cat /tmp/pub_key > ~opsadmin/.ssh/authorized_keys
 chown -R opsadmin ~opsadmin/.ssh
 chmod -R go-rwsx ~opsadmin/.ssh
+rm /tmp/pub_key
